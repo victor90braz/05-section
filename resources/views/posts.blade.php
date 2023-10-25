1b046c0954d78d9c079541540b8ecd55bce9aaba
victor braz <victor.90braz@gmail.com>
2023-10-25 12:18:30 +0200

    $attributes->merge

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,7 +9,7 @@
             @if ($posts->count() > 1)
                 <div class="lg:grid lg:grid-cols-2">
                     @foreach ($posts->skip(1) as $post)
-                        <x-post-card :post="$post" />
+                        <x-post-card :post="$post" class="bg-red-500"/>
                     @endforeach
                 </div>
             @endif
