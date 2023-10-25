4d042fc93ad4b28119c8d5f03e107a4861ba3b54
victor braz <victor.90braz@gmail.com>
2023-10-25 10:35:03 +0200

    integrate the disegn

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,22 @@
 <x-layout>
-    <div class="container" style="display: grid; grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); gap: 20px; border: 2px solid #ccc; padding: 20px;">
-        @foreach ($posts as $post)
-            <div class="grid-item" style="border: 1px solid #ccc; padding: 10px;">
-                <article>
-                    <h1 style="font-weight: bold; color: #231713; text-decoration: underline;">
-                        <a href="/posts/{{$post->slug}}" style="text-decoration: none; color: #333;">
-                            {{$post->title}}
-                        </a>
-                    </h1>
+    <php @include('post-header')
 
-                    <div>
-                        By <a href="/authors/{{ $post->author->username }}"> {{$post->author->name}} </a> in <a href="/categories/{{ $post->category->slug }}">
-                            {{ $post->category->name }}
-                         </a>
-                    </div>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-feature-card />
 
-                    <div>
-                        {!! $post->excerpt !!}
-                    </div>
-                </article>
-            </div>
-        @endforeach
-    </div>
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
 </x-layout>
+
+
+
+
