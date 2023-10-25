dd4903c4c2d44599cd84e8316d8a6b9813c247c6
victor braz <victor.90braz@gmail.com>
2023-10-25 12:31:39 +0200

    <x-posts-grid :posts="$posts" />

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,19 +4,7 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
         @if ($posts->count())
-            <x-post-feature-card :post="$posts[0]" />
-
-            @if ($posts->count() > 1)
-                <div class="lg:grid lg:grid-cols-6">
-                    @foreach ($posts->skip(1) as $post)
-                        <x-post-card
-                            :post="$post"
-                            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"
-                        />
-                    @endforeach
-                </div>
-            @endif
-
+            <x-posts-grid :posts="$posts" />
         @else
             <p>No posts yet. Please check back later.</p>
         @endif
