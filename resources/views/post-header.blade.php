90e99d26561e61254f48dc673c5f8f198c253b9d
victor braz <victor.90braz@gmail.com>
2023-10-25 16:18:55 +0200

    @click.away="show = false"

--- resources/views/post-header.blade.php
+++ resources/views/post-header.blade.php
@@ -13,19 +13,37 @@
 
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+        <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
 
-            <div x-data="{show:false}">
-                <button>Categories</button>
+            <div x-data="{ show: false }" @click.away="show = false">
+                <button
+                    class="py-2 pl-3 pr-9d text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
+                    @click="show = !show"
+                >
 
-                <div x-show="show">
-                    <a href="#">One</a>
-                    <a href="#">Two</a>
-                    <a href="#">Three</a>
+                    Categories
+
+                    <svg
+                        class="transform -rotate-90 absolute pointer-events-none"
+                        style="right: 12px;"
+                        width="22"
+                        height="22"
+                        viewBox="0 0 22 22">
+
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z"></path>
+                            <path fill="#222"d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                        </g>
+                    </svg>
+                </button>
+
+                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display: none">
+                    <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">One</a>
+                    <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">Two</a>
+                    <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">Three</a>
                 </div>
+
             </div>
-
-
         </div>
 
         <!-- Other Filters -->
