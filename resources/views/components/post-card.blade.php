5e08385a32df4893a7856f132af08293fd50e97a
victor braz <victor.90braz@gmail.com>
2023-10-25 12:02:27 +0200

    make a foreach post

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,3 +1,5 @@
+@props(['post'])
+
 <article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5">
         <div>
@@ -5,24 +7,24 @@
         </div>
 
         <div class="mt-8 flex flex-col justify-between">
-            <header>
+            <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
-                    <a href="#"
-                    class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                    style="font-size: 10px">Techniques</a>
-                    <a href="#"
-                    class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                    style="font-size: 10px">Updates</a>
+                    <a href="/categories/{{ $post->category->slug }}"
+                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+                       style="font-size: 10px">{{ $post->category->name }}</a>
                 </div>
+
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        This is a big title and it will look great on two or even three lines. Wooohoo!
+                        <a href="/posts/{{ $post->slug }}">
+                            {{ $post->title }}
+                        </a>
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                        Published <time>1 day ago</time>
-                    </span>
+                            Published <time>{{ $post->created_at->diffForHumans() }}</time>
+                        </span>
                 </div>
             </header>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,17 +2,18 @@
     <php @include('post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-feature-card :post="$posts[0]" />
+        <x-post-feature-card :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
+            @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post" />
+            @endforeach
         </div>
 
         <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
+            @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post" />
+            @endforeach
         </div>
     </main>
 </x-layout>
