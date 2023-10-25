5837d88cff1ae98f91c9ff6d81ff3146bc79e2ec
victor braz <victor.90braz@gmail.com>
2023-10-25 16:49:48 +0200

    refactor $currentCategory->is($category)

--- resources/views/post-header.blade.php
+++ resources/views/post-header.blade.php
@@ -49,7 +49,7 @@
                                 block text-left px-3 text-sm leading-6
                                 hover:bg-blue-500 focus:bg-blue-500
                                 hover:text-white focus:text-white
-                                {{ isset($currentCategory) && $currentCategory->id === $category->id ? 'bg-blue-500 text-white' : '' }}"
+                                {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}"
                             >
 
                             {{ ucwords($category->name )}}
