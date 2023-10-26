3d7e09e0a393f666d6f46d309ccee552bd1ffdac
victor braz <victor.90braz@gmail.com>
2023-10-26 13:12:50 +0200

    UPDATED session 5

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,11 @@
 <x-layout>
-    <php @include('posts-header')
+    @include ('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
         @else
-            <p>No posts yet. Please check back later.</p>
+            <p class="text-center">No posts yet. Please check back later.</p>
         @endif
-
     </main>
 </x-layout>
-
-
-
-
