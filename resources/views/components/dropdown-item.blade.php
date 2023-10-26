c0a75589b2a9fca2aaf72361fd72a20326f08445
victor braz <victor.90braz@gmail.com>
2023-10-26 11:54:50 +0200

    dropsown-item component

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,3 +1,13 @@
-<a href="/" {{ $attributes(["class" => "block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white" ])}} >
+@props(['active' => false])
+
+@php
+    $classes = "block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white";
+
+    if ($active) {
+        $classes .= ' bg-blue-500 text-white'; // Use "." for concatenation
+    }
+@endphp
+
+<a href="/" {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
